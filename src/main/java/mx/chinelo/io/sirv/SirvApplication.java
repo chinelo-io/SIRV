90839b022282e656ed11fa55e545ad2dd962c8b0
marco.io <marcoa.9722@gmail.com>
2022-01-03 01:52:44 -0600

    Initial commit, liquibase

---
+++ src/main/java/mx/chinelo/io/sirv/SirvApplication.java
@@ -0,0 +1,13 @@
+package mx.chinelo.io.sirv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SirvApplication.class, args);
+    }
+
+}
